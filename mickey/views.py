18d963d847fb6577463ac1ad795985e41b1db882
Henry Fong <foongsy@gmail.com>
2016-03-27 02:51:51 +0800

    getting close...

--- mickey/views.py
+++ mickey/views.py
@@ -8,6 +8,8 @@ from mickey.models import DailyRecord
 from wtforms import SelectField, IntegerField, validators
 from wtforms.validators import DataRequired, NumberRange
 
+app.secret_key = "hafamily"
+
 # connecting to DATABASE
 engine = create_engine(app.config['DATABASE'])
 Base = declarative_base()
@@ -19,9 +21,16 @@ s = session()
 
 # Form for custom search
 class CustomSearch(Form):
-    days = SelectField(u'How many days back?',choices=[(1,'1'),(3,'3'),(5,'5'),(10,'10')],validators=[DataRequired()])
-    turnover_threshold = IntegerField('Minimum Turnover',validators=[DataRequired(),NumberRange(min=0,message='Cannot be empty or negative')])
-    buysellratio_threshold = IntegerField('Minimum Buy/Sell Ratio',validators=[DataRequired(),NumberRange(min=0,max=100,message='Must be between 0 and 100')])
+    days = SelectField(u'How many days back?',coerce=int,
+        validators=[DataRequired('Days cannot be empty')],
+        choices=[(1,'1'),(3,'3'),(5,'5'),(10,'10')]
+    )
+    turnover_threshold = IntegerField('Minimum Turnover',
+        validators=[NumberRange(min=0,message=u'Cannot be empty or negative')]
+    )
+    buysellratio_threshold = IntegerField('Minimum Buy/Sell Ratio',
+        validators=[NumberRange(min=0,max=100,message=u'Must be between 0 and 100')]
+    )
 
 @app.route('/')
 def index():
@@ -29,11 +38,39 @@ def index():
     todaytop = s.query(DailyRecord).filter(DailyRecord.buysell_ratio > 0.5, DailyRecord.turnover >= 3000000, DailyRecord.date == last_updated).all()
     return render_template('dashboard.html',todaytop=todaytop,last_updated=last_updated)
 
-@app.route('/custom', methods=('GET','POST'))
+@app.route('/custom', methods=['GET','POST'])
 def custom():
-    form = CustomSearch(csrf_enabled=False)
+    last_updated = s.query(DailyRecord).order_by(DailyRecord.date.desc()).first().date
+    form = CustomSearch(request.form)
     if form.validate_on_submit():
-        posted = True
+        # Asks for single day data
+        if form.days.data == 1:
+            buysell_ratio = form.buysellratio_threshold.data / 100.0
+            turnover = form.turnover_threshold.data * 1000000
+            tabledata = s.query(DailyRecord).filter(DailyRecord.buysell_ratio > buysell_ratio, DailyRecord.turnover >= turnover, DailyRecord.date == last_updated).all()
+            posted=True
+            return render_template('custom.html',form=form,posted=posted,tabledata=tabledata,last_updated=last_updated)
+        # Asks for multiple days data
+        elif form.days.data > 1:
+            numofdays = form.days.data
+            predates = s.query(DailyRecord).order_by(DailyRecord.date.desc()).all()[:numofdays]
+            dates=[]
+            for d in predates:
+                dates.append(d.date)
+            print dates
+            buysell_ratio = form.buysellratio_threshold.data / 100.0
+            turnover = form.turnover_threshold.data * 1000000
+            tabledata = s.query(DailyRecord).filter(DailyRecord.buysell_ratio > buysell_ratio, DailyRecord.turnover >= turnover, DailyRecord.date in dates).all()
+            posted = True
+            return render_template('custom.html',form=form,posted=posted,tabledata=tabledata,last_updated=last_updated)
     else:
+        for e in form.errors:
+            print "FORM:" + e
+        for e in form.days.errors:
+            print "days:" + e
+        for e in form.turnover_threshold.errors:
+            print "turnover_threshold: "+e
+        for e in form.buysellratio_threshold.errors:
+            print "buysellratio_threshold: "+e
         posted = False
-    return render_template('custom.html',form=form,posted=posted)
+        return render_template('custom.html',form=form,posted=posted,last_updated=last_updated)
