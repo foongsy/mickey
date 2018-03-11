285a30ecbcdf4a64c00240f98ea45c633abdb5ec
Henry Fong <foongsy@gmail.com>
2018-03-11 12:45:34 +0800

    updated daily.sh

--- daily.sh
+++ daily.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TODAY=`date +"%Y%m%d"`
 URL="http://www.dbpower.com.hk/ch/news/mmi"
-wget -q ${URL} -O /home/sim/Bench/mickey/powerticker/${TODAY}.html
+wget -q ${URL} -O /home/sim/Bench/mickey/mickey/powerticker/${TODAY}.html
 cd /home/sim/Bench/mickey
 /home/sim/.virtualenvs/mickey/bin/python /home/sim/Bench/mickey/dailyscript.py
